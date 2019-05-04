29cc04fd537f7c90f81a47542bb7aa95ae4851c0
bluelogix <bluelogix010@gmail.com>
2019-05-03 20:34:18 -0400

    cityreader.py complete

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -26,7 +26,14 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+    with open('cities.csv', 'rt') as csv_file:
+      csv_reader = csv.reader(csv_file)
+      line_count = 0
+      for x in csv_reader:
+        if line_count == 0:
+          line_count +=1
+        else: 
+          cities.append(City(f'{x[0]}', float(x[3]), float(x[4])))
     return cities
 
 cityreader(cities)
